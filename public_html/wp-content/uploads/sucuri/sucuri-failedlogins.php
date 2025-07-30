be2b87ce727c2f2db287e4f2a5ddd77031f0a616
Navod Bopitiya <navodbopitiya@gmail.com>
2025-07-30 12:00:57 +0530

    2

---
+++ public_html/wp-content/uploads/sucuri/sucuri-failedlogins.php
@@ -0,0 +1,2 @@
+<?php exit(0); ?>
+{"user_login":"83594","attempt_time":1753697607,"remote_addr":"2401:4900:1c73:3a88:81be:c1ec:592a:33ef","user_agent":"Mozilla\/5.0 (Windows NT 6.3; arm64) AppleWebKit\/537.36 (KHTML, like Gecko) Opera\/63.0.0.0 Safari\/537.36"}
---
+++ public_html/wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1745325854;
+// updated_on=1745325854;
+exit(0);
+?>
